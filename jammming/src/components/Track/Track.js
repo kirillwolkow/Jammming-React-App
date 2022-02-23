7e88daf3ba5dea9642589564ec429377d5522452
Kirill Wolkow <kennyrogerwolf@gmail.com>
2022-02-23 11:44:46 +0100

    passed down states to render result list

--- jammming/src/components/Track/Track.js
+++ jammming/src/components/Track/Track.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './Track.css';
 
 export class Track extends React.Component {
-    renderAction(isRemoval) {
-        if (isRemoval) {
+    renderAction() {
+        if (this.props.isRemoval) {
             return <button className="Track-action">-</button>
-        } else if (!isRemoval) {
+        } else {
             return <button className="Track-action">+</button>
         }
     }
@@ -16,7 +16,7 @@ export class Track extends React.Component {
                     <h3> // track name will go here </h3>
                     <p> // track artist will go here |  track album will go here </p>
                 </div>
-                <button className="Track-action"> // + or - will go here </button>
+                    {this.renderAction()}
             </div>
         )
     }
